adb4a9a8eb0f53539e30a64a9be76db9a83781f6
SeptomiBImasakti <kucinglewat88@gmail.com>
2017-10-21 17:54:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Septomi-WebSource
---
+++ app/src/main/java/com/resepmakan/septomi/websource/MainActivity.java
@@ -0,0 +1,39 @@
+package com.resepmakan.septomi.websource;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button myButton;
+    TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myButton = (Button) findViewById(R.id.myButton);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        myButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Ion.with(getApplicationContext())
+                        .load("http://abuouday.com/hello.php")
+                        .asString()
+                        .setCallback(new FutureCallback<String>() {
+                            @Override
+                            public void onCompleted(Exception e, String result) {
+                                txtResult.setText(result);
+                            }
+                        })
+            }
+        }};
+    }
+}
